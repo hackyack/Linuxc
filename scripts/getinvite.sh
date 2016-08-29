9b11db87ca22594a1bc7683a30b411bc1f8dc86d
hisayia <hackygospal@gmail.com>
2016-08-29 07:06:47 -0700

    Update getinvite.sh

--- scripts/getinvite.sh
+++ scripts/getinvite.sh
@@ -4,6 +4,6 @@ parent_path="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
 
 key=$(cat "configs/config.json" | grep "invitationKey" | sed -e 's/^.*: *\"\([^\"]*\)\"/\1/')
 port=$(cat "configs/config.json" | grep "port" | sed -e 's/^.*: *\([0-9]*\)\,/\1/')
-invite=$(curl -s --data "invitationkey=$key" http://localhost:$port/auth?todo=invite | sed -e 's/^.*: *\"\([^\"]*\)\"}/\1/')
+invite=$ curl --data "invitationKey=MY_INVITATION_KEY" http://MY_APP.herokuapp.com/auth?todo=invite | sed -e 's/^.*: *\"\([^\"]*\)\"}/\1/')
 
 echo $invite
